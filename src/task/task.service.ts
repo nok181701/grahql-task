588928e68382f85b93688e5ceeee1d12e5255be8
祝井直樹 <nok181701@gmail.com>
2024-06-25 14:59:30 +0900

    delete task

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -34,4 +34,9 @@ export class TaskService {
       where: { id },
     });
   }
+  async deleteTask(id: number): Promise<Task> {
+    return await this.prismaService.task.delete({
+      where: { id },
+    });
+  }
 }
